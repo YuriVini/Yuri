fbc1ea88ba51c3bdf1c94c7d838aeb093a72e098
YuriVini <yuri.ybgt.yfo@gmail.com>
2024-08-14 13:10:55 -0300

    feat: ConditionalRender validate previewUrl

--- src/app/components/ProjectCard.jsx
+++ src/app/components/ProjectCard.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { CodeBracketIcon, EyeIcon } from '@heroicons/react/24/outline'
 import Link from 'next/link'
+import ConditionalRender from './ConditionalRender'
 
 const ProjectCard = ({ imgUrl, title, description, gitUrl, previewUrl }) => {
   return (
@@ -17,13 +18,15 @@ const ProjectCard = ({ imgUrl, title, description, gitUrl, previewUrl }) => {
           >
             <CodeBracketIcon className='h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-white' />
           </Link>
-          <Link
-            target='_blank'
-            href={previewUrl}
-            className='h-14 w-14 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link'
-          >
-            <EyeIcon className='h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-white' />
-          </Link>
+          <ConditionalRender condition={previewUrl !== "/"}>
+            <Link
+              target='_blank'
+              href={previewUrl}
+              className='h-14 w-14 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link'
+              >
+              <EyeIcon className='h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-white' />
+            </Link>
+          </ConditionalRender>
         </div>
       </div>
       <div className='text-white rounded-b-xl mt-3 bg-[#181818]py-6 px-4'>
