3539c45b102ab709e448ab12a80edb03aff09306
YuriVini <yuri.ybgt.yfo@gmail.com>
2024-08-14 12:59:53 -0300

    feat: add AMA project

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -98,6 +98,16 @@ const projectsData = [
     gitUrl: 'https://github.com/YuriVini/Proffy',
     previewUrl: '/',
   },
+  {
+    id: 9,
+    title: 'AMA - Ask Me Anything',
+    description:
+      'AMA is a Web and Mobile application designed to help people in their questions. Therefore, this application offers users the possibility of registering a room, being able to add questions, reactions and mark as answered.',
+    image: '/images/projects/10.png',
+    tag: ['All', 'Web', 'Backend'],
+    gitUrl: 'https://github.com/YuriVini/ask-me-anything',
+    previewUrl: '/',
+  },
 ]
 
 const ProjectsSection = () => {
