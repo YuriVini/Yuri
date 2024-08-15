9f64df3fe07741826bdfdcad8769db6e81e10e59
YuriVini <yuri.ybgt.yfo@gmail.com>
2024-08-15 14:03:11 -0300

    feat: changing layout page to ts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { PropsWithChildren } from 'react'
 import './globals.css'
 import { Inter } from 'next/font/google'
 
@@ -8,7 +9,7 @@ export const metadata = {
   description: 'Portfolio of Yuri Vinicius a Mobile Engineer',
 }
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: Readonly<PropsWithChildren>) {
   return (
     <html lang='en'>
       <body className={inter.className}>{children}</body>
