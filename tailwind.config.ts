923a831dfcf22ddc50875ec68f2e6ff514b09385
YuriVini <yuri.ybgt.yfo@gmail.com>
2024-08-15 14:21:26 -0300

    feat: changing Tailwind to ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
-/** @type {import('tailwindcss').Config} */
-const colors = require('tailwindcss/colors')
-module.exports = {
+import type { Config } from 'tailwindcss'
+import colors  from 'tailwindcss/colors'
+
+export default {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -22,4 +23,4 @@ module.exports = {
     },
   },
   plugins: [],
-}
+} satisfies Config
