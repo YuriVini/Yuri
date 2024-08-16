74a296fe409008b3506dadacf8846e5380b9c475
YuriVini <yuri.ybgt.yfo@gmail.com>
2024-08-16 10:45:05 -0300

    feat: changing EmailSection to ts

--- src/app/components/EmailSection.tsx
+++ src/app/components/EmailSection.tsx
@@ -1,9 +1,10 @@
 'use client'
 import React from 'react'
-import GithubIcon from '../../../public/github-icon.svg'
-import LinkedinIcon from '../../../public/linkedin-icon.svg'
 import Link from 'next/link'
 import Image from 'next/image'
+
+import GithubIcon from '../../../public/github-icon.svg'
+import LinkedinIcon from '../../../public/linkedin-icon.svg'
 
 const EmailSection = () => {
 
