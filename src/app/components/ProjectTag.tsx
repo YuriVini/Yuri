7ba7a3670df45a523b2689908bda4a27dd96c023
YuriVini <yuri.ybgt.yfo@gmail.com>
2024-08-17 11:00:39 -0300

    feat: add Types to ProjectTag

--- src/app/components/ProjectTag.tsx
+++ src/app/components/ProjectTag.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 
-const ProjectTag = ({ name, onClick, isSelected }) => {
+interface ProjectTagProps {
+  name: string;
+  onClick: (tag: string) => void;
+  isSelected: boolean;
+}
+
+const ProjectTag = ({ name, onClick, isSelected }: ProjectTagProps) => {
   const buttonStyles = isSelected
     ? "text-white border-primary-500"
     : "text-[#ADB7BE] border-slate-600 hover:border-white";
