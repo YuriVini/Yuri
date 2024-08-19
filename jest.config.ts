3d7d3f64fe87a575ec3f70196fbfc0ef723e4b24
YuriVini <yuri.ybgt.yfo@gmail.com>
2024-08-18 21:18:32 -0300

    feat: jest ts config

--- jest.config.ts
+++ jest.config.ts
@@ -1,10 +1,22 @@
 import type { Config } from 'jest';
+import nextJest from 'next/jest'
+
+const createJestConfig = nextJest({
+  dir: './',
+})
 
 const config: Config = {
   clearMocks: true,
   collectCoverage: true,
   coverageDirectory: "coverage",
+  "collectCoverageFrom": [
+    "**/src/*.{ts,tsx,js,jsx}",
+  ],
   testEnvironment: "jsdom",
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/',
+  },
+  modulePaths: ['<rootDir>/src/'],
 };
 
-export default config;
+export default createJestConfig(config);
