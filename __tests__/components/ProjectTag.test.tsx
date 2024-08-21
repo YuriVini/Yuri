91ea1afe16ec5f5f54214146d05043afbf35dda5
YuriVini <yuri.ybgt.yfo@gmail.com>
2024-08-21 17:44:44 -0300

    feat: ProjectTag test

---
+++ __tests__/components/ProjectTag.test.tsx
@@ -0,0 +1,19 @@
+
+import { render, screen, fireEvent } from '@testing-library/react'
+
+import ProjectTag from '@/app/components/ProjectTag'
+
+const onClickMock = jest.fn()
+
+describe('ProjectTag', () => {
+    it('render Texts and press button', () => {
+      render(<ProjectTag isSelected name='Test' onClick={onClickMock} />)
+        
+      const buttonTitle = screen.getByText('Test')
+
+      fireEvent.click(buttonTitle)
+        
+      expect(onClickMock).toHaveBeenCalled()
+    })
+
+  })
