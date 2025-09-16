3d07919c781077b47273bc96b244e58c712011d2
YuriVini <yuri.ybgt.yfo@gmail.com>
2025-09-16 16:17:00 -0300

    add conditional when does not have git url

--- src/app/components/ProjectCard.tsx
+++ src/app/components/ProjectCard.tsx
@@ -11,7 +11,13 @@ interface ProjectCardProps {
   previewUrl?: string
 }
 
-const ProjectCard = ({ imgUrl, title, description, gitUrl, previewUrl }: ProjectCardProps) => {
+const ProjectCard = ({
+  imgUrl,
+  title,
+  description,
+  gitUrl,
+  previewUrl,
+}: ProjectCardProps) => {
   return (
     <div>
       <div
@@ -19,19 +25,21 @@ const ProjectCard = ({ imgUrl, title, description, gitUrl, previewUrl }: Project
         style={{ background: `url(${imgUrl})`, backgroundSize: 'cover' }}
       >
         <div className='overlay items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500 '>
-          <Link
-            target='_blank'
-            href={gitUrl}
-            className='h-14 w-14 mr-2 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link'
-          >
-            <CodeBracketIcon className='h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-white' />
-          </Link>
-          <ConditionalRender condition={previewUrl !== "/"}>
+          <ConditionalRender condition={gitUrl !== '/'}>
+            <Link
+              target='_blank'
+              href={gitUrl}
+              className='h-14 w-14 mr-2 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link'
+            >
+              <CodeBracketIcon className='h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-white' />
+            </Link>
+          </ConditionalRender>
+          <ConditionalRender condition={previewUrl !== '/'}>
             <Link
               target='_blank'
               href={previewUrl}
               className='h-14 w-14 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link'
-              >
+            >
               <EyeIcon className='h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-white' />
             </Link>
           </ConditionalRender>
